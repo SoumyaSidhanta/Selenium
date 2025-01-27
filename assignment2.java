664b9328ca82589e92f27a3c760fbca9021a89e3
Sou <163277537+SoumyaSidhanta@users.noreply.github.com>
2025-01-27 18:16:42 +0530

    Add files via upload
    HOW TO FILL UP THE FORM USING SELENIUM ?

---
+++ assignment2.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
+public class assignment2 {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("web driver.chrome.driver", "C:/Users/mohan/OneDrive/Documents/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
+
+        /*
+        *   FORM FILL UP
+        * */
+
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+
+        driver.findElement(By.cssSelector("input[class*='ng-untouched']")).sendKeys("Soumya Sidhanta Mohanta");
+        driver.findElement(By.cssSelector("div[class='form-group'] input[name='email']")).sendKeys("mohantasoumyasidhanta@gmail.com");
+        driver.findElement(By.id("exampleInputPassword1")).sendKeys("Mohanta@442005");
+
+        driver.findElement(By.id("exampleCheck1")).click();
+
+        // FOR DROPDOWN
+        WebElement dropdown = driver.findElement(By.id("exampleFormControlSelect1"));
+        Select dropdowns = new Select(dropdown);
+        dropdowns.selectByVisibleText("Female");
+        System.out.println(dropdowns.getFirstSelectedOption().getText());
+
+
+        driver.findElement(By.name("inlineRadioOptions")).click();
+        driver.findElement(By.name("bday")).sendKeys("04/04/2005");
+        driver.findElement(By.cssSelector("input[class*='btn-success']")).click();
+        
+        //GRABBING ALERT MESSAGE
+        System.out.println(driver.findElement(By.cssSelector("div[class*='alert-success']")).getText());
+        driver.quit();
+
+    }
+}
